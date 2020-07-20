240da68c1a09f463b3755a651993a3ba3a2005e4
kamataryo <kamataryo@users.noreply.github.com>
2020-07-20 19:57:46 +0900

    gunzip

--- src/lib.ts
+++ src/lib.ts
@@ -1,15 +1,13 @@
 // @ts-ignore
 import MBTiles from "@mapbox/mbtiles";
-import fs from "fs";
+import zlib from "zlib";
 
 const mbtilesPath = process.env.MOUNT_PATH! + "/tiles.mbtiles";
-const testTextPath = process.env.MOUNT_PATH! + "/test.txt";
 
 export const errorResponse = (statusCode: number, message: string) =>
   JSON.stringify({
     statusCode,
-    headers: { "Access-Control-Allow-Origin": "*" },
-    body: message,
+    body: { message },
   });
 
 export const getTile = (
@@ -22,11 +20,11 @@ export const getTile = (
       if (error) {
         reject(error);
       } else {
-        mbtiles.getTile(z, x, y, (error: any, data: any, headers: any) => {
+        mbtiles.getTile(z, x, y, (error: any, data: Buffer, headers: any) => {
           if (error) {
             reject(error);
           } else {
-            resolve(data);
+            resolve(zlib.gunzipSync(data));
           }
         });
       }
